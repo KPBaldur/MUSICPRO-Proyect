a7f20c49ada60e61e68e88ad9b4c15dd068154b4
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-05-13 17:01:21 -0400

    Modificando APP

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 # 0 ejecutamos pip install flask flask-sqlalchemy flask-migrate flask-cors
-# 1 Crear modelos
-# 2 importamos las librerias de flask
+# 1 Crear modelos 
+# 2 Importamos las librerias de flask
 
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
@@ -8,7 +8,7 @@ from sqlalchemy import desc
 from models import db, Prodcuto, Cliente, Vendedor
 from flask_cors import CORS, cross_origin
 
-# 3 Se crea la instancia para la app
+# 3 instanciamos la app 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Conten-Type'
@@ -16,13 +16,11 @@ app.url_map.strict_slashes = False
 app.config['DEBUG'] = False
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'html:///database.db'
 
 db.init_app(app)
 
 Migrate(app, db)
 
-
-
-if __name__ == '__main__':
-    app.run(port=5500, debug=True)
+# 5 Creamos la ruta por defecto para saber si mi app esta funcionando
+# 6 Ejecutamos el comando en la consola: python app.py o python3 app.py o py app.py y revisamos nuestro navegador
