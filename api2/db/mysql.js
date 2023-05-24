0b60572c1da1665b061d7c38b23591d828204cc6
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-05-24 17:05:26 -0400

    Instalacion de
    Instancias para uso de mysql.
    Creacion de cionexion a la base de datos.

--- api2/db/mysql.js
+++ api2/db/mysql.js
@@ -1,11 +1,38 @@
 const mysql = require('mysql');
 const config = require('../config');
 
-const test = {
-    id: 1,
-    nombre: 'juan',
-    edad: 43,
+const dbconfig = {
+    host: config.mysql.hotst,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
 }
+
+let conexion; 
+
+function conexmysql(){
+    conexion = mysql.createConnection(dbconfig)
+
+    conexion.conect((err) => {
+        if(err){
+            console.log('[db err]', err);
+            setTimeout(conexmysql, 200);
+        }else{
+            console.log('DB Conectada!')
+        }
+    });
+
+    conexion.on('error', err=>{
+        console.log('[db err]', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            conexmysql();
+        }else{
+            throw err;
+        }
+    })
+}
+
+conexmysql();
 
 function productosTabla(tabla){
     return test ;
