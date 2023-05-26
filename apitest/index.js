b0494fff7fbc06dc28b9d62a8d1e5d4db60c20c8
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-05-26 00:58:39 -0400

    Update index.js

--- apitest/index.js
+++ apitest/index.js
@@ -7,41 +7,7 @@ app.get('/', (req, res) =>{
   res.send('Hola prueba de servidor');
 });
 
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filter');
-})
 
-app.get('/productos/:id', (req, res) =>{
-  const { id } = req.params;
-  res.json({
-    name: 'Producto1',
-    
-    price: 1000
-    });
-});
-
-app.get('/productos', (req, res) =>{
-  const productos = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++){
-    productos.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      imagen: faker.image.imageUrl(),
-    })
-  }
-  res.json(productos);
-});
-
-
-app.get('/categories/:categoryId/productos/:productId', (req, res) =>{
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
 
 app.get('/Administradores', (req, res) =>{
   res.json([  
