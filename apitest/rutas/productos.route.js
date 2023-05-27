54258209ad44d2e894f99a71459f20c79259f1fd
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-05-27 00:56:39 -0400

    Update productos.route.js

--- apitest/rutas/productos.route.js
+++ apitest/rutas/productos.route.js
@@ -8,13 +8,28 @@ const router = express.Router();
 router.get('/', (req, res) =>{
     const productos = [];
     const { size } = req.query;
-    const limit = size || 10;
+    const limit = size || 1;
     for (let index = 0; index < limit; index++){
-      productos.push({
-        name: faker.commerce.productName(),
-        price: parseInt(faker.commerce.price(), 10),
-        imagen: faker.image.imageUrl(),
-      })
+      productos.push([
+        {
+          id: '001',
+          name: 'Guitarra Acustica Mcqueen',
+          precio: '120000',
+          stock: '15'
+        },
+        {
+          id: '002',
+          name: 'Guitarra-Electrica-Jonson',
+          precio: '150000',
+          stock: '4'
+        },
+        {
+          id: '003',
+          name: 'Guitarra Emerson',
+          precio: '180000',
+          stock: '1'
+        },
+      ])
     }
     res.json(productos);
   });
@@ -33,7 +48,7 @@ router.get('/:id', (req, res) =>{
     } else {
       res.status(200).json({
         name: 'Producto1',
-        price: 1000
+        precio: 1000
         });
     }
   });
@@ -42,12 +57,38 @@ router.get('/:id', (req, res) =>{
 
 //Metodo para crear productos
 router.post('/', (req, res) =>{
+  try {
+    const{ id, name, precio, stock} = req.body;
+    if (!id || !name || !precio || !stock ){
+      return res.status(400).json({ error: 'Faltan datos del producto'});
+    }
+    const nuevoProducto = {
+      id,
+      name,
+      precio,
+      stock
+    };
+
+    res.status(201).json({
+      message: 'Producto agregado exitosamente',
+      producto: nuevoProducto,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ha ocurrido un error al agregar el producto'});
+  }
+});
+
+/* Ruta original POST 
+router.post('/', (req, res) =>{
   const body = req.body;
   res.status(201).json({
     message: 'created',
     data: body
   });
 });
+*/
+
 
 //Metodo para actualizar parcialmente productos (actualizar un solo campo)
 router.patch('/:id', (req, res) =>{
