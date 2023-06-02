db7949204a9f3fb4ec965cc5c26b9968fef1fab5
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-06-02 17:02:25 -0400

    Creacion de validador
    Se crea modulo de validaciones.

---
+++ apitest/middlewares/validator.handler.js
@@ -0,0 +1,15 @@
+const boom = require ('@hapi/boom');
+
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const error = schema.validate(data);
+    if (error) {
+      boom.badRequest(error);
+      next(boom.badRequest(error));
+    }
+    next();
+  }
+}
+
+module.exports = validatorHandler;
---
+++ apitest/schemas/products.schema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(15);
+const price = Joi.number().integer().min(1000);
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+
+
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema }
