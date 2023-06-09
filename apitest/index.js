c27365bb6d6c63f4656810ed0ce36631e2e767a4
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-06-09 13:06:50 -0400

    Se implementan los CORS
    Se implementan los cors para litimar el uso a demas usuarios y consultas.
    
    Se mantiene activo el CORS para la consulta publica y se mantiene comentada el CORS que limita la consulta solo a algunos usuarios

--- apitest/index.js
+++ apitest/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./rutas');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -7,6 +8,24 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
+
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) =>{
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback (new Error('No esta permitido, vaya a chupar limon'));
+    }
+  }
+};
+app.use(cors(options));
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
